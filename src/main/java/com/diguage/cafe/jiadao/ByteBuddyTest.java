93b6258b9d547309bce73134491c9b510bc8881e
diguage <leejun119@gmail.com>
2022-07-19 00:10:53 +0800

    更新依赖，增加测试代码

--- src/main/java/com/diguage/cafe/jiadao/ByteBuddyTest.java
+++ src/main/java/com/diguage/cafe/jiadao/ByteBuddyTest.java
@@ -1,4 +1,43 @@
 package com.diguage.cafe.jiadao;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.NamingStrategy;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import org.junit.jupiter.api.Test;
+
 public class ByteBuddyTest {
+
+    @Test
+    public void test1() {
+        DynamicType.Unloaded<Object> dynamicType = new ByteBuddy()
+                .subclass(Object.class)
+                .make();
+    }
+
+    @Test
+    public void test2() {
+        DynamicType.Unloaded<Object> dynamicType = new ByteBuddy()
+                .subclass(Object.class)
+                .name("com.diguage.cafe.jiadao.demo.Type")
+                .make();
+    }
+
+    @Test
+    public void test3() {
+        DynamicType.Unloaded<Object> dynamicType = new ByteBuddy()
+                .with(new NamingStrategy.AbstractBase() {
+                    @Override
+                    protected String name(TypeDescription superClass) {
+                        return "com.diguage.cafe.buddy." + superClass.getTypeName();
+                    }
+
+//                    @Override
+//                    public String subclass(TypeDescription.Generic superClass) {
+//                        return "com.diguage.cafe.buddy." + superClass.getTypeName();
+//                    }
+                })
+                .subclass(Object.class)
+                .make();
+    }
 }
