bc7b3a3274bf2c796d87d9674cb5b1501f0d1e77
diguage <leejun119@gmail.com>
2022-09-05 21:30:58 +0800

    增加新内容

---
+++ src/main/java/Example.java
@@ -0,0 +1,136 @@
+/**
+ * 字节码示例代码
+ *
+ * @author D瓜哥 · https://www.diguage.com
+ */
+public class Example {
+    static {
+        float f = 0.0F + 5.0F;
+    }
+
+    {
+        int i = 1 + 4;
+    }
+
+    {
+        long l = 2 + 5;
+    }
+
+    public Example() {
+
+    }
+
+    public Example(int i) {
+
+    }
+//    /**
+//     * 测试 boolean 型示例
+//     */
+//    public boolean testBoolean() {
+//        return true;
+//    }
+//
+//    /**
+//     * 测试 byte 型示例
+//     */
+//    public byte testByte() {
+//        // '0' == 0x30 == 48
+//        return '0';
+//    }
+//
+//    /**
+//     * 测试 char 型示例
+//     */
+//    public char testChar() {
+//        // '0' == 0x30 == 48
+//        return '0';
+//    }
+//
+//    /**
+//     * 测试 short 型示例
+//     */
+//    public short testShort() {
+//        // '0' == 0x30 == 48
+//        return 32767;
+//    }
+
+//    /**
+//     * 操作码 ldc2_w 示例
+//     */
+//    public long testLdc2_w() {
+//        // 替换为long 和 double 类型除上述内容提到的之外的值，
+//        // 编译查看结果
+//        return 2L;
+//    }
+
+//    /**
+//     * 操作码 ldc 示例
+//     */
+//    public float testLdc() {
+//        // 替换为除上述内容提到的 int 和 float 之外的值，或者字符串
+//        // 编译查看结果
+//        return 1.1F;
+//    }
+
+//    /**
+//     * 操作码 sipush 示例
+//     */
+//    public int testSipush() {
+//        // 替换为 -32768 ~ -129 和 128 ~ 32767 之间的整数，
+//        // 编译查看结果
+//        return -32768;
+//    }
+
+//    /**
+//     * 操作码 bipush 示例
+//     */
+//    public int testBipush() {
+//        // 替换为 -128 ~ -2 和 6 ~ 127 之间的整数，
+//        // 编译查看结果
+//        return 6;
+//    }
+
+//    /**
+//     * 操作码 dconst_<d> 示例
+//     */
+//    public double testDconst() {
+//        // 替换为 1.0，编译查看结果
+//        return 0.0;
+//    }
+
+//    /**
+//     * 操作码 fconst_<f> 示例
+//     */
+//    public float testFconst() {
+//        // 依次替换为 1.0F 和 2.0F，编译查看结果
+//        return 0.0F;
+//    }
+
+//    /**
+//     * 操作码 lconst_<l> 示例
+//     */
+//    public void testLconst() {
+//        long l0 = 0L;
+//        long l1 = 1L;
+//    }
+
+//    /**
+//     * 操作码 iconst_<i> 示例
+//     */
+//    public void testIconst() {
+//        int im1 = -1;
+//        int i0 = 0;
+//        int i1 = 1;
+//        int i2 = 2;
+//        int i3 = 3;
+//        int i4 = 4;
+//        int i5 = 5;
+//    }
+
+//    /**
+//     * 操作码 aconst_null 示例
+//     */
+//    public Object testAconst() {
+//        return null;
+//    }
+}
